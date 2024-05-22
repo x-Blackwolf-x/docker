8adc8567f9bcf2d46bbe11c347631903a68add0a
Blackwolf <140268124+x-Blackwolf-x@users.noreply.github.com>
2024-05-22 17:39:43 +0200

    Updated teams.js

--- Docker9/api/routes/teams.js
+++ Docker9/api/routes/teams.js
@@ -3,9 +3,16 @@ const app = express();
 const Team = require('../models/team');
 
 app.get('/', (req, res) => {
-  Team.find()
+  Team.find({})
       .then((teams) => res.status(200).send(teams))
       .catch((err) => res.status(500).send(err));
 });
-
+app.post('/', (req, res) => {
+  const team = new Team(req.body);
+  team.save()
+      .then(() => res.status(201).send(teams))
+      .catch((err) => res
+          .status(400)
+          .send(err));
+});
 module.exports = app;
